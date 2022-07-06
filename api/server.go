cd99ad63cf17be4456767bf72add196098c2a825
M1racle-Heen <190109003@stu.sdu.edu.kz>
2022-07-06 20:58:15 +0600

    created a new HTTP server and  setup routing

---
+++ api/server.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	db "github.com/M1racle-Heen/simple_order_pizza/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/customers", server.createCustomer)
+
+	server.router = router
+	return server
+}
